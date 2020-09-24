2e56ab99be53d182325b63534dd432638f7a2b88
maanefisk <62015771+maanefisk@users.noreply.github.com>
2020-09-24 16:16:33 +0200

    Initial commit

---
+++ Nummertest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Nummertest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            while (true)
+            {
+                string pinCodeSelection = Console.ReadLine();
+                int n;
+                bool isNumeric = Int32.TryParse(pinCodeSelection, out n);
+
+                if (isNumeric == false)
+                {
+                    Console.WriteLine("Please enter a valid numerical 4 digit value!");
+                }
+                else {Console.WriteLine("Your PIN is" + "\n" + pinCodeSelection + "\n" + n);}
+            }
+
+
+            
+        }
+    }
+}
